6470c36db613f01b362dd94b9afd811416698066
Tonic0124 <guilhermeacmaciel0606@gmail.com>
2026-06-14 01:58:57 +0100

    Ciração do primeiro mapa do jogo

---
+++ assets/TIlesetMaps/Map1/background/Backgroundforest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="backgroundforest" tilewidth="32" tileheight="32" tilecount="144" columns="16">
+ <image source="../../../../../../../OneDrive/Ambiente de Trabalho/Pixel Art Tiles and Backgrounds - Woods/Backgrounds/WOODS - Second.png" width="512" height="288"/>
+</tileset>
---
+++ assets/TIlesetMaps/Map1/background/background_deco.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="background_deco" tilewidth="32" tileheight="32" tilecount="144" columns="16">
+ <image source="OneDrive/Ambiente de Trabalho/Pixel Art Tiles and Backgrounds - Woods/Backgrounds/BACKGROUND.png" width="512" height="288"/>
+</tileset>
